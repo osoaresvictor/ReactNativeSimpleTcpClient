becc8dacc4445d03a0788cfc25de3490e5aa6e56
Victor Soares <victor_soares@live.com>
2018-10-09 16:23:20 -0300

    Update library index structure

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import { NativeModules } from 'react-native';
 const { RNSocketTcpClient } = NativeModules;
 
 export default class TcpClient {
-	sendData(address, port, stringData) {
-		RNSocketTcpClient.sendData(address, port, stringData);
+	constructor(address, port){
+		this.address = address;
+		this.port = port;
+	}
+
+	sendData(stringData) {
+		RNSocketTcpClient.sendData(this.address, this.port, stringData);
 	}
 };
